0c57bd2eac70dd30b99c67b6975295d6dc053967
Xeirah07 <suyatsarah519@gmail.com>
2023-09-29 02:35:28 +0800

    Added initial files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    
+        Student s = new Student("Sarah Mae Suyat", "IT21C", "Trust the timing of your life");
+        
+        System.out.println("Name: " + s.getName());
+        System.out.println("Section: " + s.getSection());
+        System.out.println("Motto: " + s.getMotto());
+    }
+}
